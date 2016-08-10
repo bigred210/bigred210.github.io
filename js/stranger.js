c8d2c8d360f8ce27e8ad14fa56d010dde7c7c391
Jerod West <jerod.west@gmail.com>
2016-08-10 14:28:26 -0500

    added css stranger folder, img folder stranger and js

---
+++ js/stranger.js
@@ -0,0 +1,5 @@
+    $('.roomR').delay(3000).show(0).delay(1500).hide(0);
+    $('.roomU').delay(4000).show(0).delay(1000).hide(0);
+    $('.roomN').delay(5000).show(0).delay(1500).hide(0);
+    $('#bg').delay(6500).hide(0);
+    $('#home').delay(6510).show(0);
