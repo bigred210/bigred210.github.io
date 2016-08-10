b98ee6bab31a9f482c4f8f20eead79dc165024d6
Jerod West <jerod.west@gmail.com>
2016-08-10 10:34:14 -0500

    added :     js folder and moved files over     pokemon img added

---
+++ js/pokemon.js
@@ -0,0 +1,13 @@
+    $('.storm5').delay(2000).show(0).delay(100).hide(0);
+    $('.storm5').delay(500).show(0).delay(100).hide(0);
+    $('.storm5').delay(500).show(0).delay(100).hide(0);
+    $('.storm5').delay(500).show(0).delay(100).hide(0);
+    $('.storm5').delay(500).show(0).delay(100).hide(0);
+    $('.storm5').delay(500).show(0).delay(100).hide(0);
+    $('.storm5').delay(500).show(0).delay(100).hide(0);
+    $('.storm5').delay(500).show(0).delay(100).hide(0);
+    $('.pikachu').delay(6990).fadeIn(1800).fadeOut(1000).hide(0);
+    $('.logo').delay(10700).show(0);
+    $('.jumbotron').delay(10700).show(0);
+    $('.catch').delay(10700).show(0);
+    $('.links').delay(10700).show(0);
